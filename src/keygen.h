8f98835d88138b98ee3b347dd1e843fd9f18043c
AngryFender <teaBiscuitPerson@gmail.com>
2026-07-20 19:37:10 +0100

    Randomly selecting the combinations of the letters in the KeyGen class

--- src/keygen.h
+++ src/keygen.h
@@ -2,21 +2,38 @@
 
 #include "./interfaces/ikeygen.h"
 #include "entity.h"
+#include <unordered_set>
+#include <iostream>
 
 class KeyGen final : public IKeyGen
 {
 public:
     ~KeyGen() override = default;
+
+    KeyGen(): combos_(ALL_COMBINATION)
+    {};
+
     std::vector<Key> generate(const std::vector<std::pair<float, float>>& points, const ScreenInfo& screen) override 
     {
         std::vector<Key> keys;
         keys.reserve(points.size());
 
-        
+        int random_index = 0;
+        for (const auto& point : points)
+        {
+            random_index = rand() % (TOTAL_COMBINATION-1) ;
+            keys.push_back(std::move(combos_[random_index]));
+        }
+
         return keys;
     }
 
     void refresh()override 
     {
+        combos_ = ALL_COMBINATION;
     };
+
+private:
+    PossibleCombo combos_;
+
 };
