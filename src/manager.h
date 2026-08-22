8c574e8a90bc0e39418e6ef3a92ae7d65666dcd2
AngryFender <teaBiscuitPerson@gmail.com>
2026-08-22 23:49:43 +0100

    Added a manager template to ochestrate interactions between each components

---
+++ src/manager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+template <
+    typename Renderer,
+    typename ScreenCapturer,
+    typename MouseClicker
+>
+class Manager {
+
+public:
+    Manager( Renderer renderer, ScreenCapturer capturer, MouseClicker clicker):
+        renderer_(std::move(renderer),
+        screen_capturer_(std::move(capturer), 
+        mouse_clicker_(std::move(clicker))){
+
+    }
+
+    ~Manager() {
+
+    }
+
+private:
+    Renderer renderer_;
+    ScreenCapturer screen_capturer_;
+    MouseClicker mouse_clicker_;
+
+};
