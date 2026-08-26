7f9c9d727d8cf35c6f4fa1b16f3e73f7d7130bfc
AngryFender <teaBiscuitPerson@gmail.com>
2026-08-26 23:49:37 +0100

    Adding skeleton for the main loop for now

--- src/manager.h
+++ src/manager.h
@@ -11,16 +11,28 @@ public:
     Manager(Renderer&& renderer, ScreenCapturer&& capturer, MouseClicker&& clicker)
         : renderer_(std::move(renderer),
           screen_capturer_(std::move(capturer),
-          mouse_clicker_(std::move(clicker)) {
+          mouse_clicker_(std::move(clicker)),
+          shutdown{ false }
+    {
+    }
+
+    void start() {
+        while (renderer.shutdown() || shutdown) {
+            //TODO logic inside the main loop
+        }
+    }
+
+    void stop() {
+
     }
 
     ~Manager() {
-
+        shutdown = false;
     }
 
 private:
     Renderer renderer_;
     ScreenCapturer screen_capturer_;
     MouseClicker mouse_clicker_;
-
+    bool shutdown;
 };
