fdbf0893c470bd1aeb4fdae8dfec36721e3c3e40
AngryFender <teaBiscuitPerson@gmail.com>
2026-08-29 23:52:44 +0100

    Adding MatchEngine type to the Manager template. Also moving the coordinates and keys away from the manager into the inner components

--- src/manager.h
+++ src/manager.h
@@ -1,32 +1,35 @@
 #pragma once
 
 #include "entity.h"
+#include <vector>
 
 template <
     typename Renderer,
     typename KeyboardListener,
     typename ScreenCapturer,
-    typename MouseClicker
+    typename MouseClicker,
+    typename MatchEngine,
 >
 class Manager {
 
 public:
-    Manager(Renderer&& renderer, KeyboardListener&& listener, ScreenCapturer&& capturer, MouseClicker&& clicker)
+    Manager(Renderer&& renderer,
+        KeyboardListener&& listener,
+        ScreenCapturer&& capturer,
+        MouseClicker&& clicker,
+        MatchEngine&& match_engine,
+        )
         : renderer_(std::move(renderer),
           keyboard_listener_(std::move(listener)),
           screen_capturer_(std::move(capturer),
           mouse_clicker_(std::move(clicker)),
+          match_engine_(std::move(match_engine)),
           shutdown{ false }
     {
         //init
-        coordinates_.reserve(TOTAL_COMBINATION);
-        keys_.reserve(TOTAL_COMBINATION);
-
         renderer_.init();
         capturer_.init();
         keyboard_listener_.init();
-
-        //TODO main loop
 
     }
 
@@ -55,10 +58,6 @@ private:
     KeyboardListener keyboard_listener_;
     ScreenCapturer screen_capturer_;
     MouseClicker mouse_clicker_;
+    MatchEngine match_engine_;
     bool shutdown;
-
-    std::vector<std::pair<float, float>> coordinates_;
-    std::vector<Key> keys_;
-    ScreenInfo screen_;
-
 };
