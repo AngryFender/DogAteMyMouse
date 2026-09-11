115aa649d397378ee94c67b87a7e22454c0b3a0c
AngryFender <teaBiscuitPerson@gmail.com>
2026-09-11 23:56:49 +0100

    Added a base class for KeyGeneration that will be used for compile time polymorphism

--- src/interfaces/ikeygen.h
+++ src/interfaces/ikeygen.h
@@ -10,3 +10,12 @@ public:
     virtual std::vector<Key> generate(const std::vector<std::pair<float, float>>& coordinates, const ScreenInfo& screen) = 0;
 };
 
+
+class KeyGenBase {
+public:
+    template <typename Self>
+    auto generate(this const Self& self, const std::vector<std::pair<float,float>>& coordinates, const ScreenInfo& screen) {
+        return self.generate_impl(coordinates, screen);
+    }
+};
+
